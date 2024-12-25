4a04848f4b0f52a59bd45b57f5e02adb105e6bd3
Bryan Owens <bryanowens012@gmail.com>
2024-12-24 21:38:04 -0800

    Install chakra-ui

--- app/packages/frontend/next.config.ts
+++ app/packages/frontend/next.config.ts
@@ -29,6 +29,7 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     typedRoutes: true,
+    optimizePackageImports: ["@chakra-ui/react"],
   },
 };
 
--- app/packages/frontend/pages/_app.tsx
+++ app/packages/frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/global.css";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { useState } from "react";
@@ -12,7 +13,9 @@ const App = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <ChakraProvider value={defaultSystem}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </QueryClientProvider>
   );
 };
